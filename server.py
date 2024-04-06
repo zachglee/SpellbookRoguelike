4a8c14511925f98345d3aa5151d835f90ee74537
Zachary Lee <zacharygowlee@gmail.com>
2024-04-06 10:54:37 -0400

    serving the html from the server itself

--- server.py
+++ server.py
@@ -1,67 +1,26 @@
-import os
-import time
-from typing import Union
+
 from main import GameStateV2
-from pydantic import BaseModel
 import uuid
 
 from fastapi import FastAPI, WebSocket
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from starlette.responses import FileResponse
 from utils import ws_input
 from sound_utils import ws_play_sound
 
 app = FastAPI()
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
-
 app.mount("/sounds", StaticFiles(directory="assets/sounds"), name="sounds")
 
-runs = {}
+@app.get("/game")
+async def read_index():
+    return FileResponse('index.html')
 
-@app.get("/")
-async def get():
-    return HTMLResponse(html)
+runs = {}
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-
-    await ws_play_sound("apply-poison.mp3", websocket)
-    await ws_play_sound("apply-armor.mp3", websocket)
-    await ws_play_sound("gold-consumer-cast.mp3", websocket)
 
     run_id = await ws_input("run id > ", websocket)
     if run_id not in runs:
@@ -75,8 +34,4 @@ async def websocket_endpoint(websocket: WebSocket):
     print(f"----------- BEGINNING {player_id} on run {run_id}!")
     ret = await game_state.play(player_id, websocket)
     print(ret)
-
-    # while True:
-    #     data = await websocket.receive_text()
-    #     await websocket.send_text(f"You said: {data}\r$ ")
 
