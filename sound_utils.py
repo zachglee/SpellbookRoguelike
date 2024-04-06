d6cb8f52e4f011786f551a31cc490a764b0cb710
Zachary Lee <zacharygowlee@gmail.com>
2024-04-06 11:08:22 -0400

    fix

--- sound_utils.py
+++ sound_utils.py
@@ -1,15 +1,15 @@
 import asyncio
-# from pygame import mixer
+from pygame import mixer
 import os
 
 # mixer.init()
 
-# def play_sound(filename, channel=0):
-#   full_path = f"assets/sounds/{filename}"
-#   if os.path.exists(full_path):
-#     mixer.Channel(channel).play(mixer.Sound(full_path))
-#   else:
-#     print(f"Sound file {full_path} not found")
+def play_sound(filename, channel=0):
+  full_path = f"assets/sounds/{filename}"
+  if os.path.exists(full_path):
+    mixer.Channel(channel).play(mixer.Sound(full_path))
+  else:
+    print(f"Sound file {full_path} not found")
 
 async def ws_play_sound(filename, websocket, channel=0):
   await websocket.send_text(f"play_sound:{filename}")
